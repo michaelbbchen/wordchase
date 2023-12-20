b9a955f60254d06791af462851bac4981280aa2f
Michael Chen <mdchenmail@gmail.com>
2023-12-19 17:37:38 -0800

    button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './Home';
+import Room from './Room';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <Router>
+          <Routes>
+            <Route path='/' element = {<Home/>}/>
+            <Route path='/room' element = {<Room/>}>
+              <Route path=":roomId" element={<Room />} />
+            </Route>
+          </Routes>
+        </Router>
       </header>
     </div>
   );
